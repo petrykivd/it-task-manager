b4f4d53cb72bdc3f376a5136ced0a433060d65d6
Dmytro Petrykiv <petrykiv.dmytro19@gmail.com>
2023-11-22 12:47:04 +0200

    Created models and migrations

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}: {self.position}"
+
+    # def get_absolute_url(self):
+    #     return reverse("")
+
+
+class Task(models.Model):
+
+    PRIORITY_CHOICES = [
+        ("urgent", "Urgent"),
+        ("critical", "Critical"),
+        ("medium", "Medium"),
+        ("asap", "ASAP (As Soon As Possible)"),
+    ]
+
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=30,
+                                choices=PRIORITY_CHOICES,
+                                default="medium")
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
+
+    def __str__(self):
+        return f"Task: {self.name}, type: ({self.task_type})"
